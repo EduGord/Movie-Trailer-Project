e9b313defee78e2d78bd9826a535f11d63229f7f
MadHuman <edu_gordilho@hotmail.com>
2018-07-22 15:44:01 -0300

    inserting docstring

--- media.py
+++ media.py
@@ -4,6 +4,11 @@ import webbrowser
 
 
 class Movie:
+    """The Movie class initializes a Movie object.
+    Attributes:
+        title: Movie Title (string)
+        poster: URL of the Movie Poster
+        trailer: URL of the Movie Trailer"""
     def __init__(self, movie_title, movie_poster_url, movie_trailer_url):
         self.title = movie_title
         self.poster = movie_poster_url
